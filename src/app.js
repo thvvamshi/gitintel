8716fc215f2cb18ffcb064667091e176c7583c9b
thvvamshi <bodavamshikumar2002@gmail.com>
2026-06-09 19:44:42 +0530

    feat: setup Express server and health check endpoint

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(morgan("dev"));
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "GitIntel API Running",
+  });
+});
+
+module.exports = app;
